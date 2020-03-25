c900f6d5e8ef273000d221d0e46ab81ed4aed2a2
dzen <benoit.calvez@polyconseil.fr>
2020-03-25 11:02:04 +0100

    fix pamqp version for tests on travis

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setuptools.setup(
         'aioamqp',
     ],
     install_requires=[
-        'pamqp>=2.2.0,<3',
+        'pamqp==3.0.0a6', # TODO(bcalvez): for tests purpose, until 3.0 is released
     ],
     classifiers=[
         "Development Status :: 4 - Beta",
